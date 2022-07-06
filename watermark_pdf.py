2b6f689e295720c02af9f45cf9c8898e1a718937
JaggedPurpose <junwonsuh@gmail.com>
2022-07-06 10:00:06 -0500

    Basic frame of the project

---
+++ watermark_pdf.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+
+import re
+import email
+import mimetypes
+import os
+from docx2pdf import convert
+
+
+def main():
+
+
+def convert_doc():
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
