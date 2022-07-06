59d54e8fff09f74f67623315cc94a31e9bf14b8a
JaggedPurpose <junwonsuh@gmail.com>
2022-07-06 13:22:58 -0500

    Added a function to get the md5 checksum of the converted pdf file

--- watermark_pdf.py
+++ watermark_pdf.py
@@ -5,26 +5,34 @@ import email
 import mimetypes
 import os
 from docx2pdf import convert
-import comtypes.client
+import hashlib
 
 
 def main():
-    doc = input("Example: /home/<user>/Desktop/file.docx"
+    doc = input("Example: C:\<user>\Desktop\\file.docx"
                 "\nPlease provide the .docx file to be converted into a PDF as the above example: ")
+    requester = input("What is the name of the person requesting for the document? ")
     convert_doc(doc)
+    print(md5sum(doc))
 
 
 def convert_doc(doc_file):
-    # wdFormatPDF = 17
-    # word_doc = comtypes.client.CreateObject("Word.Application")
-    # doc_file = word_doc.Documents.Open(doc_file)
-    # doc_file.SaveAs(pdf_name, FileFormat=wdFormatPDF)
-    # doc_file.Close()
-    # word_doc.Quit()
-    # convert(doc_file)
+    # Separate the path/file and extension
     pdf_name = os.path.splitext(doc_file)[0] + ".pdf"
+    # convert the doc_file to pdf with the pdf extension
     convert(doc_file, pdf_name)
     print("Docx file has been converted to PDF")
+
+
+def md5sum(pdf_file):
+    # call out the correct file, which in this case would be the converted pdf
+    pdf_file = os.path.splitext(pdf_file)[0] + ".pdf"
+    # open the pdf file
+    with open(pdf_file, 'rb') as pdf:
+        # read the pdf file
+        file_pdf = pdf.read()
+        checksum = hashlib.md5(file_pdf).hexdigest()
+        return checksum
 
 
 if __name__ == "__main__":
